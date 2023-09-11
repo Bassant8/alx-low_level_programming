748ea7cdd3013d56f6f05cdf3dbec6a20231fd44
bassant <bassantadel855@gmail.com>
2023-09-10 19:02:52 -0700

    pp

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
@@ -6,15 +7,14 @@
  */
 int main(void)
 {
-    int digit;
+    int num;
 
-    for (digit = 0; digit < 10; digit++)
+    for (num = 0; num <= 9; num++)
     {
-        if (digit != 4 && digit != 9)
-            putchar(digit + '0');
+        write(1, &num, 1);
     }
 
-    putchar('\n');
+    write(1, "\n", 1);
 
     return (0);
 }
