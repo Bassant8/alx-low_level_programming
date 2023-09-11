ff21ad19fbc98bcabf8863770000bf819781b601
bassant <bassantadel855@gmail.com>
2023-09-10 19:33:53 -0700

    pp

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
 #include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
-    ssize_t len;
-    len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
     return (1);
 }
 
