c5e910fecba59956e288f7dfb212b0334b3b3ce1
bassant <bassantadel855@gmail.com>
2023-09-16 03:09:32 -0700

    pp

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints "_putchar" followed by a newline character
+ * and returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("_putchar\n");
+    return (0);
+}
