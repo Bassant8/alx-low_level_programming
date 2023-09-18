56dea50c64b8ee0176b9fbd20b0afb573242df40
bassant <bassantadel855@gmail.com>
2023-09-18 01:10:00 -0700

    pp

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,18 +5,17 @@
 *@str: A pointer to a string
 *
 *Description: This function prints every other character of a string
-*starting with the first character and followed by a new line.
+*              starting with the first character and followed by a new line.
 */
 void puts2(char *str)
 {
-	int i = 0;
+    int i = 0;
 
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i += 2;
-	}
+    while (str[i] != '\0')
+    {
+        _putchar(str[i]);
+        i += 2;
+    }
 
-	_putchar('\n');
+    _putchar('\n');
 }
-
